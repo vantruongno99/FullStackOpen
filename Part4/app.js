4b226a3fa3ca9fe36edd8a879d139146b85bf783
Van Truong Phan <vantruongno99@gmail.com>
2021-11-14 23:17:36 +1100

    seperate from front-end

--- Part4/app.js
+++ Part4/app.js
@@ -20,8 +20,8 @@ mongoose.connect(config.MONGODB_URI)
     logger.error('error connecting to MongoDB:', error.message);
   });
 
+  
 app.use(cors());
-app.use(express.static('build'));
 app.use(express.json());
 app.use(middleware.requestLogger);
 app.use(middleware.tokenExtractor);
